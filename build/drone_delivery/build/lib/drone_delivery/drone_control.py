bb1f5661c5b9202635c98c391d70286768bd70da
Loris Giuliani <loris04.giuliani@gmail.com>
2024-10-29 12:16:26 +0000

    I'm not sure this fixed anything

--- build/drone_delivery/build/lib/drone_delivery/drone_control.py
+++ build/drone_delivery/build/lib/drone_delivery/drone_control.py
@@ -1,5 +1,6 @@
 import rclpy
 from geometry_msgs.msg import Twist, Point
+from example_interfaces.srv import SetBool
 from controller import Robot
 
 #HALF_DISTANCE_BETWEEN_WHEELS = 0.045
@@ -39,14 +40,20 @@ class DroneDriver:
         self.targetZ = 0
         self.targetX = 0
         self.targetY = 0
+        self.itemGrabbed = False
+        self.launchable = False
+        self.atTarget = False
 
         self.__target_twist = Twist()
 
         # Configure ROS interface
         rclpy.init(args=None)
         self.subscription = rclpy.create_node('driver_'+self.name) 
-        self.subscription.create_subscription(Twist, 'cmd_vel_' + self.name, self.__cmd_vel_callback, 1)
+        self.subscription.create_subscription(Twist, self.name + '/cmd_vel', self.__cmd_vel_callback, 1)
         self.subscription.create_subscription(Point, 'goto_robot', self.listener_position, 1)
+
+        self.robotservice = rclpy.create_node(self.name + "_listen")
+        picker_service = self.robotservice.create_service(SetBool, 'item_picker', self.item_picker)
 
     def __cmd_vel_callback(self, twist):
         self.__target_twist = twist
@@ -57,6 +64,11 @@ class DroneDriver:
         droneVelocity = self.gps.getSpeed()
         gyroValues = rollVelocity, pitchVelocity, yawVelocity = self.gyro.getValues()
         return internalComputerValues, gpsValues, gyroValues, droneVelocity
+
+    def item_picker(self, request, response):
+        response = SetBool()
+        response.data = self.launchable
+        return response
 
     def bindValue(self, value):
         return min(max(value, -1), 1)
@@ -98,79 +110,83 @@ class DroneDriver:
     def step(self): #having difficulties with the name 'step'...io
         rclpy.spin_once(self.subscription, timeout_sec=0)
         self.__target_twist = Twist()
-        #self.targetX = 2
-        #self.targetY = 2
-        #self.targetZ = 2
-        print(self.targetX, self.targetY, self.targetZ)
-        #print(self.gps.getValues()[2] - self.targetZ)
+        if self.launchable:
+            #self.targetX = 2
+            #self.targetY = 2
+            #self.targetZ = 2
+            #print(self.targetX, self.targetY, self.targetZ)
+            #print(self.gps.getValues()[2] - self.targetZ)
 
-        #Where is the drone?
-        intComVal, gpsVal, gyroVal, droneVelocity = self.locateDrone()
+            #Where is the drone?
+            intComVal, gpsVal, gyroVal, droneVelocity = self.locateDrone()
 
-        #Read target from topic?
+            #Read target from topic?
 
-        forward = None
-        right = None
+            forward = None
+            right = None
 
-        # X Positioning
-        if abs(gpsVal[0] - self.targetX) > 0.25:
-            xMovement = gpsVal[0] - self.targetX
-            if xMovement < 0:
-                right = True
+            # X Positioning
+            if abs(gpsVal[0] - self.targetX) > 0.25:
+                xMovement = gpsVal[0] - self.targetX
+                if xMovement < 0:
+                    right = True
+                else:
+                    right = False
+            # Y Positioning
+            if abs(gpsVal[1] - self.targetY) > 0.25:
+                yMovement = gpsVal[1] - self.targetY
+                if yMovement < 0:
+                    forward = True
+                else:
+                    forward = False
+            
+            
+            #print("Roll, Pitch, Yaw: " + str(intComVal[0]) + " " + str(intComVal[1]) + " " + str(intComVal[2]))
+            #print("PosX, PosY, PosZ: " + str(gpsVal[0]) + " " + str(gpsVal[1]) + " " + str(gpsVal[2]))
+            
+
+            roll_input = 50 * clamp(intComVal[0], -1, 1) + gyroVal[0] 
+            pitch_input = 30.0 * clamp(intComVal[1], -1, 1) + gyroVal[1]
+            yaw_input = 2 * (self.__target_twist.angular.z - gyroVal[2])
+            clamped_difference_altitude = clamp(self.targetZ - gpsVal[2] + 0.6, -1, 1)
+            vertical_input = 3.0 * clamped_difference_altitude**3.0
+
+
+            if gpsVal[2] > self.targetZ - 0.25 and abs(self.targetX - gpsVal[0]) > 0.5:
+                #print("Forward, Right: "+ str(forward) + " " + str(right)) 
+                if right:
+                    pitch_input = pitch_input - 1
+                else:
+                    pitch_input = pitch_input + 1
+                if forward:
+                    roll_input = roll_input - 1
+                else:
+                    roll_input = roll_input + 1
+
+            front_left_motor_input = 70 + vertical_input - yaw_input + pitch_input - roll_input
+            front_right_motor_input = 70 + vertical_input + yaw_input + pitch_input + roll_input
+            rear_left_motor_input = 70 + vertical_input + yaw_input - pitch_input - roll_input
+            rear_right_motor_input = 70 + vertical_input - yaw_input - pitch_input + roll_input
+            self.frp.setVelocity(-front_right_motor_input) # Front Right
+            self.flp.setVelocity(front_left_motor_input) # Front Left
+            self.rrp.setVelocity(rear_right_motor_input) # Rear Right
+            self.rlp.setVelocity(-rear_left_motor_input) # Rear Left
+
+            #self.__propellerList[0].setVelocity(-100) # Front Right
+            #self.__propellerList[1].setVelocity(100) # Front Left
+            #self.__propellerList[2].setVelocity(100) # Rear Right
+            #self.__propellerList[3].setVelocity(-100) # Rear Left
+        else:
+            #print("picking up item...")
+            if self.gps.getValues()[2] < 0.25:
+                self.launchable = True
             else:
-                right = False
-        # Y Positioning
-        if abs(gpsVal[1] - self.targetY) > 0.25:
-            yMovement = gpsVal[1] - self.targetY
-            if yMovement < 0:
-                forward = True
-            else:
-                forward = False
-        
-        
-        #print("Roll, Pitch, Yaw: " + str(intComVal[0]) + " " + str(intComVal[1]) + " " + str(intComVal[2]))
-        #print("PosX, PosY, PosZ: " + str(gpsVal[0]) + " " + str(gpsVal[1]) + " " + str(gpsVal[2]))
-         
-
-        roll_input = 50 * clamp(intComVal[0], -1, 1) + gyroVal[0] 
-        pitch_input = 30.0 * clamp(intComVal[1], -1, 1) + gyroVal[1]
-        yaw_input = 2 * (self.__target_twist.angular.z - gyroVal[2])
-        clamped_difference_altitude = clamp(self.targetZ - gpsVal[2] + 0.6, -1, 1)
-        vertical_input = 3.0 * clamped_difference_altitude**3.0
-
-
-        if gpsVal[2] > self.targetZ - 0.25 and abs(self.targetX - gpsVal[0]) > 0.5:
-            #print("Forward, Right: "+ str(forward) + " " + str(right)) 
-            if right:
-                pitch_input = pitch_input - 1
-            else:
-                pitch_input = pitch_input + 1
-            if forward:
-                roll_input = roll_input - 1
-            else:
-                roll_input = roll_input + 1
-
-        front_left_motor_input = 70 + vertical_input - yaw_input + pitch_input - roll_input
-        front_right_motor_input = 70 + vertical_input + yaw_input + pitch_input + roll_input
-        rear_left_motor_input = 70 + vertical_input + yaw_input - pitch_input - roll_input
-        rear_right_motor_input = 70 + vertical_input - yaw_input - pitch_input + roll_input
-        self.frp.setVelocity(-front_right_motor_input) # Front Right
-        self.flp.setVelocity(front_left_motor_input) # Front Left
-        self.rrp.setVelocity(rear_right_motor_input) # Rear Right
-        self.rlp.setVelocity(-rear_left_motor_input) # Rear Left
-
-        #self.__propellerList[0].setVelocity(-100) # Front Right
-        #self.__propellerList[1].setVelocity(100) # Front Left
-        #self.__propellerList[2].setVelocity(100) # Rear Right
-        #self.__propellerList[3].setVelocity(-100) # Rear Left
+                print("the drone isn't launchable")
 
 def main():
     #rclpy.init(args=None)
     robotControl = DroneDriver()
-    target = [[2, 2, 0]]
-    robotControl.goToLocation(target)
-    #rclpy.spin(robotControl)
-    #robotControl.moveRobot()
+    rclpy.spin(robotControl)
     robotControl.destroy_node()
     rclpy.shutdown()
 
--- install/drone_delivery/lib/python3.12/site-packages/drone_delivery/location_publisher.py
+++ install/drone_delivery/lib/python3.12/site-packages/drone_delivery/location_publisher.py
@@ -2,35 +2,61 @@ import rclpy
 from geometry_msgs.msg import Point
 from rclpy.node import Node
 from std_msgs.msg import String
+from example_interfaces.srv import SetBool
 
 
-class MinimalPublisher(Node):
+class DirectionPublisher(Node):
     def __init__(self, data):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Point, 'goto_robot', 1)
         timer_period = 1 # seconds
-        self.data = data
-        self.create_timer(timer_period, self.location_pub)
+        self.data = data[0]
+        self.announce = False
+        if self.announce:
+            self.create_timer(1, self.location_pub)
+    
+    def initalise_direciton(self, booleanDirection):
+        self.announce = booleanDirection
 
     def location_pub(self):
-        if len(self.data) != 0:
-            self.instruction = self.data.pop()
         msg = Point()
-        msg.x = float(self.instruction[0])
-        msg.y = float(self.instruction[1])
-        msg.z = float(self.instruction[2])
+        msg.x = float(self.data[0])
+        msg.y = float(self.data[1])
+        msg.z = float(self.data[2])
         self.publisher_.publish(msg)
+
+class MetadataClient(Node):
+    def __init__(self):
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(SetBool, 'item_picker')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('item picker service not available, waiting again...')
+        self.req = SetBool.Request()
+
+    def send_request(self):
+        return self.cli.call_async(self.req)
 
 
 def main(args=None):
     rclpy.init(args=args)
     data = [[2,2,0]]
-    minimal_publisher = MinimalPublisher(data)
-    rclpy.spin(minimal_publisher)
+    dir_pub = DirectionPublisher(data)
+    robot_data = MetadataClient()
+    future = robot_data.send_request()
+    rclpy.spin_until_future_complete(robot_data, future)
+    response = future.result()
+    while response.data == False:
+        print("robot not ready to takeoff")
+    
+    dir_pub.initalise_direciton(True)
+    rclpy.spin(dir_pub)  
+    
+
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    dir_pub.destroy_node()
+    robot_data.destory_node()
     rclpy.shutdown()
 
 
