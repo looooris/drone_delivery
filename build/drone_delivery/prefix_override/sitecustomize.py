1c29586bdd6875e225920dc164768c01c41642f0
Loris Giuliani <loris04.giuliani@gmail.com>
2024-11-16 01:27:16 +0000

    remove junk (for the last tome hopefully)

--- build/drone_delivery/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/loris/Documents/Projects/ROS2/drone_delivery_main/install/drone_delivery'
